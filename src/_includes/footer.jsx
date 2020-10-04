f85dc65976b969930b9922df689a0490bbd64d63
Lucas Picollo <lucaspicolloo@gmail.com>
2020-10-04 02:41:48 -0300

    feat: add shared header and footer

---
+++ src/_includes/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <p>©2020 | Footer</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/_includes/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <p>pikonha.dev | Powered by Next.js</p>
+    </div>
+  );
+}
+
+export default Header;
