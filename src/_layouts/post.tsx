35e010f2ad66d3ce2ee93892f1b990c9ef6b96d9
Lucas Picollo <lucaspicolloo@gmail.com>
2020-10-15 20:14:49 -0300

    feat: code markdown rendering

--- src/_layouts/post.tsx
+++ src/_layouts/post.tsx
@@ -1,11 +1,20 @@
 import ReactMarkdown from "react-markdown";
 import DefaultLayout from "@layouts/default";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
 interface PostLayoutProps {
   title: string;
   content: string;
   date: string;
 }
+
+const CodeBlock = ({ language, value }) => {
+  return (
+    <SyntaxHighlighter language={language} customStyle={{ fontSize: "15px" }}>
+      {value}
+    </SyntaxHighlighter>
+  );
+};
 
 export default function PostLayout({
   title,
@@ -20,7 +29,11 @@ export default function PostLayout({
           <small className="text-sm text-gray-500 mb-2">{date}</small>
         </div>
 
-        <ReactMarkdown source={content} />
+        <ReactMarkdown
+          source={content}
+          renderers={{ code: CodeBlock }}
+          escapeHtml={false}
+        />
       </article>
     </DefaultLayout>
   );
--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const BrandLogo: React.FC = () => {
   return (
     <Link href="/">
-      <span className="cursor-pointer font-bold text-xl hover:bg-yellow-primary px-2 py-1 rounded-sm">
+      <span className="cursor-pointer font-bold text-lg hover:bg-yellow-primary px-2 py-1 rounded-sm">
         {"<Pikonha.dev />"}
       </span>
     </Link>
